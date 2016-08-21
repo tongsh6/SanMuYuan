f4cb4024e6afdc895ef6566f84b4fb91b8b32808
Loong <tongsh6@qq.com>
2016-08-21 23:45:27 +0800

    添加主页面控制器 添加菜单控制器 添加系统用户控制器

--- src/main/java/com/loong/jfinalwebdemo/controller/HelloController.java
+++
@@ -1,9 +0,0 @@
-package com.loong.jfinalwebdemo.controller;
-
-import com.jfinal.core.Controller;
-
-public class HelloController extends Controller {
-	public void index() {
-		renderText("Hello J world.");
-	}
-}
