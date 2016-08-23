29144fac21c82417375bad2e41f7025beeb54099
tongsh6 <tongsh6@qq.com>
2016-08-23 21:13:59 +0800

    添加根据用户级别加载菜单的方法

--- src/main/java/com/nstc/sanmuyuan/service/MenuService.java
+++ src/main/java/com/nstc/sanmuyuan/service/MenuService.java
@@ -3,7 +3,10 @@ package com.nstc.sanmuyuan.service;
 import java.util.List;
 
 import com.nstc.sanmuyuan.model.MenuTree;
+import com.nstc.sanmuyuan.model.SysUser;
 
 public interface MenuService {
 	public List<MenuTree> getAllMenu();
+
+	public List<MenuTree> getMenuByUser(SysUser user);
 }
