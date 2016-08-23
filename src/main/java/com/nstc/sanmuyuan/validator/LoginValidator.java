63d64506e39f72610a848c0b375a14ba965c9cee
tongsh6 <tongsh6@qq.com>
2016-08-23 21:16:20 +0800

    优化了登录时验证消息提示顺序

--- src/main/java/com/nstc/sanmuyuan/validator/LoginValidator.java
+++ src/main/java/com/nstc/sanmuyuan/validator/LoginValidator.java
@@ -7,8 +7,8 @@ public class LoginValidator extends Validator {
 
 	@Override
 	protected void validate(Controller c) {
-		validateRequiredString("sysUser.uno", "errorMsg", "请输入用户名！");
 		validateRequiredString("sysUser.pwd", "errorMsg", "请输入密码！");
+		validateRequiredString("sysUser.uno", "errorMsg", "请输入用户名！");
 	}
 
 	@Override
