ef7350f7dee4bb543344a4fd2a54c924d25516c2
tongsh6 <tongsh6@qq.com>
2016-08-23 21:15:02 +0800

    添加根据用户级别加载菜单的方法

--- src/main/java/com/nstc/sanmuyuan/service/impl/MenuServiceImpl.java
+++ src/main/java/com/nstc/sanmuyuan/service/impl/MenuServiceImpl.java
@@ -3,12 +3,17 @@ package com.nstc.sanmuyuan.service.impl;
 import java.util.List;
 
 import com.nstc.sanmuyuan.model.MenuTree;
+import com.nstc.sanmuyuan.model.SysUser;
 import com.nstc.sanmuyuan.service.MenuService;
 
 public class MenuServiceImpl implements MenuService {
 	@Override
 	public List<MenuTree> getAllMenu() {
-		// TODO Auto-generated method stub
-		return MenuTree.dao.find("select * from menu_tree order by sortno");
+		return MenuTree.dao.list();
+	}
+
+	@Override
+	public List<MenuTree> getMenuByUser(SysUser user) {
+		return MenuTree.dao.getMenuByUser(user);
 	}
 }
