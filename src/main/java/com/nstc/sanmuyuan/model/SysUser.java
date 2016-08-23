b9d08945c7bb0763cca5bbaf951a573ddf7997cc
tongsh6 <tongsh6@qq.com>
2016-08-23 21:12:46 +0800

    添加了list方法

--- src/main/java/com/nstc/sanmuyuan/model/SysUser.java
+++ src/main/java/com/nstc/sanmuyuan/model/SysUser.java
@@ -1,5 +1,7 @@
 package com.nstc.sanmuyuan.model;
 
+import java.util.List;
+
 import com.nstc.sanmuyuan.model.base.BaseSysUser;
 
 /**
@@ -13,8 +15,16 @@ public class SysUser extends BaseSysUser<SysUser> {
 	 * 根据用户名获取对象
 	 */
 	public SysUser login(SysUser sysUser) {
-		String sql = "select userid,uname from sys_user where uno=? and pwd=?";
-		return super.findFirst(sql, sysUser.getUno(), sysUser.getPwd());
+		String sql = "select userid,uname,utype from sys_user where uno=? and pwd=?";
+		return findFirst(sql, sysUser.getUno(), sysUser.getPwd());
+	}
+
+	/**
+	 * 根据用户名获取对象
+	 */
+	public List<SysUser> list() {
+		String sql = "select userid,uno,uname from sys_user order by userid";
+		return find(sql);
 	}
 
 }
