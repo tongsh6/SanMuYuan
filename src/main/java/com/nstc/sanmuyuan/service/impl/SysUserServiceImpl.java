4fe4ca1011f6959edfa705f0425f3aba34a9b390
tongsh6 <tongsh6@qq.com>
2016-08-23 21:15:25 +0800

    添加了list方法

--- src/main/java/com/nstc/sanmuyuan/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/nstc/sanmuyuan/service/impl/SysUserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.nstc.sanmuyuan.service.impl;
 
+import java.util.List;
+
 import com.nstc.sanmuyuan.model.SysUser;
 import com.nstc.sanmuyuan.service.SysUserService;
 
 public class SysUserServiceImpl implements SysUserService {
 	public SysUser login(SysUser sysUser) {
 		return sysUser.login(sysUser);
+	}
+
+	@Override
+	public List<SysUser> list() {
+		return SysUser.dao.list();
 	}
 }
