c5278ab499ae6b96f6ac52df753cafa59cf280e8
tongsh6 <tongsh6@qq.com>
2016-08-23 21:14:20 +0800

    添加了list方法

--- src/main/java/com/nstc/sanmuyuan/service/SysUserService.java
+++ src/main/java/com/nstc/sanmuyuan/service/SysUserService.java
@@ -1,7 +1,11 @@
 package com.nstc.sanmuyuan.service;
 
+import java.util.List;
+
 import com.nstc.sanmuyuan.model.SysUser;
 
 public interface SysUserService {
 	public SysUser login(SysUser sysUser);
+
+	public List<SysUser> list();
 }
